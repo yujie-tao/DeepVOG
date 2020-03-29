99fb416ba52c5c9c4c0c93c43eb93c4c04f9d2a8
yujie-tao <yujietao@outlook.com>
2020-03-28 23:20:52 -0400

    revise setup file

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-
-
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -13,28 +11,27 @@ setuptools.setup(
     description="Deep VOG for gaze estimation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pydsgz/DeepVOG",
+    url="https://github.com/yujie-tao/DeepVOG",
     license="GNU General Public License v3.0",
     packages=setuptools.find_packages(),
     classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
     ],
-    package_data = {
-        'deepvog':['model/*.py', 'model/*.h5'],
+    package_data={
+        "deepvog": ["model/*.py", "model/*.h5"],
         #'model_weights':['model/*.h5'],
-        
     },
-    python_requires='>=3.5',
-    install_requires=['numpy>=1.12',
-                      'scikit-video>=1.1.0',
-                      'scikit-image>=0.14.0',
-                      'tensorflow-gpu>=1.12.0',
-                      'keras>=2.2.4'],
-
-    
+    python_requires=">=3.5",
+    install_requires=[
+        "numpy>=1.12",
+        "scikit-video>=1.1.0",
+        "scikit-image>=0.14.0",
+        "tensorflow-gpu>=1.12.0",
+        "keras>=2.2.4",
+    ],
 )
